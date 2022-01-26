f216bc68014b598ef8a0dbb912ac287ffbf8834d
brentleejohnson <brentleejohnson73@gmail.com>
2022-01-26 12:44:55 +0200

    Testing error solutions

--- src/App.js
+++ src/App.js
@@ -21,8 +21,9 @@ function App(props) {
     />
   ));
 
-  const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
-  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  console.log(taskList.length);
+  // const tasksNoun = taskList.length !== 1 ? "tasks" : "task";
+  // const headingText = `${taskList.length} ${tasksNoun} remaining`;
 
   return (
     <div className="todoapp stack-large">
@@ -33,7 +34,7 @@ function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading">3 Tasks remaining</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
